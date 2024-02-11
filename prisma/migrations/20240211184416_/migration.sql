09ba7e3fe47da0b37a5c56ca7a8d0276b639b5d7
Num Joseph <konum5437@gmail.com>
2024-02-11 10:45:59 -0800

    NEW

--- prisma/migrations/20240211184416_/migration.sql
+++ prisma/migrations/20240211184416_/migration.sql
@@ -1,6 +1,6 @@
 -- CreateTable
 CREATE TABLE "User" (
-    "id" SERIAL NOT NULL,
+    "id" TEXT NOT NULL,
     "fullName" TEXT NOT NULL,
     "password" TEXT NOT NULL,
     "email" TEXT NOT NULL,
@@ -12,14 +12,14 @@ CREATE TABLE "User" (
 
 -- CreateTable
 CREATE TABLE "expenses" (
-    "id" SERIAL NOT NULL,
+    "id" TEXT NOT NULL,
     "itermName" TEXT NOT NULL,
     "quantity" INTEGER NOT NULL DEFAULT 0,
     "amount" DOUBLE PRECISION NOT NULL DEFAULT 0.00,
     "totalAmount" DOUBLE PRECISION NOT NULL DEFAULT 0.00,
     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updatedAt" TIMESTAMP(3) NOT NULL,
-    "userId" INTEGER,
+    "userId" TEXT,
 
     CONSTRAINT "expenses_pkey" PRIMARY KEY ("id")
 );
