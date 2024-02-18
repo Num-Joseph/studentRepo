d03148bc35055f720fe4592a3454d68a7f33eff3
Num Joseph <konum5437@gmail.com>
2024-02-18 19:13:41 +0000

    current commit

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 
 const bodyParser = require("body-parser");
 const bcrypt = require("bcrypt");
+const cors = require("cors");
 const jwt = require("jsonwebtoken");
 
 const PORT = 3000;
@@ -11,6 +12,7 @@ const appRouter = require("./routes/index");
 
 app.use(bodyParser.json());
 app.use("/api", appRouter);
+app.use(cors({ origin: true, credentials: true }));
 console.log("test");
 
 // This is where we listen to our server
